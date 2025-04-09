ed1187675a29fe167e8b75046f5b47d7d394ed4c
Tauseef Ameen <123378504+tauseef-ameen@users.noreply.github.com>
2025-04-09 08:09:21 +0200

    Delete Lab1/conference-room-booking-systems directory

--- Lab1/conference-room-booking-systems/src/main/java/com/manning/workout/reservation/RoomReservationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.manning.workout.reservation;
-
-import java.util.List;
-
-public interface RoomReservationRepository {
-    List<RoomReservation> findAll();
-
-    RoomReservation save(RoomReservation roomReservation);
-}
