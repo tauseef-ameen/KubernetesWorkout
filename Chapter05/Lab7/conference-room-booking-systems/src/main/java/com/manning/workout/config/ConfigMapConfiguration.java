e2dd5853336d0159c12f49b1b18488c26f942ee9
tauseef-ameen <tauseef.125002@gmail.com>
2025-05-16 20:48:22 +0200

    added @Setter

--- Chapter05/Lab7/conference-room-booking-systems/src/main/java/com/manning/workout/config/ConfigMapConfiguration.java
+++ Chapter05/Lab7/conference-room-booking-systems/src/main/java/com/manning/workout/config/ConfigMapConfiguration.java
@@ -1,11 +1,13 @@
 package com.manning.workout.config;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @Getter
+@Setter
 @ConfigurationProperties(prefix = "manning.workout")
 public class ConfigMapConfiguration {
     private String building1;
