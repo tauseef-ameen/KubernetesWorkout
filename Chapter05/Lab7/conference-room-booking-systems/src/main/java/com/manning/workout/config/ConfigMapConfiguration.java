48a7768cb3c10999bd0372b538638ba5eee5dd43
tauseef-ameen <tauseef.125002@gmail.com>
2025-05-16 20:42:08 +0200

    updated properties

--- Chapter05/Lab7/conference-room-booking-systems/src/main/java/com/manning/workout/config/ConfigMapConfiguration.java
+++ Chapter05/Lab7/conference-room-booking-systems/src/main/java/com/manning/workout/config/ConfigMapConfiguration.java
@@ -1,14 +1,12 @@
 package com.manning.workout.config;
 
 import lombok.Getter;
-import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ConfigurationProperties(prefix = "manning.workout")
 @Getter
-@Setter
+@ConfigurationProperties(prefix = "manning.workout")
 public class ConfigMapConfiguration {
     private String building1;
     private String building2;
