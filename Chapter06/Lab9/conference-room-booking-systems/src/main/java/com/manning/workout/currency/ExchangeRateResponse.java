b6c1a1cd274fadef420369380846d2229726bfd5
tauseef-ameen <tauseef.125002@gmail.com>
2025-07-08 21:42:04 +0200

    Refactoring

--- Chapter06/Lab9/conference-room-booking-systems/src/main/java/com/manning/workout/currency/ExchangeRateResponse.java
+++ Chapter06/Lab9/conference-room-booking-systems/src/main/java/com/manning/workout/currency/ExchangeRateResponse.java
@@ -1,10 +1,7 @@
 package com.manning.workout.currency;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
 import java.util.Map;
 
-@JsonIgnoreProperties(ignoreUnknown = true)
 public record ExchangeRateResponse(
         String result,
         String base_code,
