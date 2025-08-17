3f0b89de31338214fb5ccbf09a81ade050dc98a2
tauseef-ameen <tauseef.125002@gmail.com>
2025-08-17 14:11:17 +0200

    updated Room state client

--- Chapter05/Lab8/room-state-client-quarkus-microservice/src/main/java/org/acme/state/RoomStateClient.java
+++ Chapter05/Lab8/room-state-client-quarkus-microservice/src/main/java/org/acme/state/RoomStateClient.java
@@ -1,17 +1,25 @@
 package org.acme.state;
 
+import jakarta.inject.Inject;
 import jakarta.inject.Singleton;
 
 import java.util.List;
 
 @Singleton
 public class RoomStateClient {
+    private static final String AVAILABLE = "Available";
+    private static final String MAINTENANCE = "Available";
+    private static final String OCCUPIED = "Occupied";
+    @Inject
+    RoomConfiguration config;
 
     public List<RoomState> allRoomStates() {
-        return List.of(new RoomState(1, "Available"),
-                new RoomState(11, "Maintenance"),
-                new RoomState(22, "Available"),
-                new RoomState(33, "Occupied"),
-                new RoomState(44, "Available"));
+        return List.of(
+                new RoomState(config.room1(), AVAILABLE),
+                new RoomState(config.room2(), MAINTENANCE),
+                new RoomState(config.room3(), AVAILABLE),
+                new RoomState(config.room4(), OCCUPIED),
+                new RoomState(config.room5(), AVAILABLE)
+        );
     }
 }
